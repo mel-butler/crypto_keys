0922c67d0ef9f5afaf5f2ec6a7b6af42c454cec9
imelda <20259344@studentmail.ul.ie>
2022-06-07 14:20:56 +0200

    removed function method

--- src/keys.java
+++ src/keys.java
@@ -18,12 +18,6 @@ public class keys {
         // find the modulus
         m = modulus(p, q);
 
-        // find the function
-        f = function(p, q);
-
-        // find the public encrypt format by selecting any number that is relatively prime
-        // aka when they share no common factors other than one.
-        public_encrypt_ex = generateCoPrime(f);
 
         System.out.println("public key. say Alice's: ");
         System.out.println("encryption exponent = " + public_encrypt_ex);
@@ -63,9 +57,7 @@ public class keys {
         return a*b;
     }
 
-    public static int function(int a, int b){
-        return (a - 1) * (b - 1);
-    }
+
 
     // calculate the greatest common divider using euclid's method
     public static int gcd(int a, int b) {
